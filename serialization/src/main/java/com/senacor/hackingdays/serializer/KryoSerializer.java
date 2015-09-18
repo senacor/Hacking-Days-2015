b10dbd25786a614b3487610fa2f7db81b54deb10
Alasdair Collinson <blalasaadri@googlemail.com>
2015-09-18 13:33:42 +0200

    RangeSerializer registriert

--- serialization/src/main/java/com/senacor/hackingdays/serializer/KryoSerializer.java
+++ serialization/src/main/java/com/senacor/hackingdays/serializer/KryoSerializer.java
@@ -7,12 +7,18 @@ import akka.serialization.JSerializer;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import com.senacor.hackingdays.serialization.data.Range;
 
 public class KryoSerializer extends JSerializer {
 
+    private Kryo kryo = new Kryo();
+
+    public KryoSerializer() {
+        kryo.addDefaultSerializer(Range.class, new RangeSerializer());
+    }
+
     @Override
     public Object fromBinaryJava(byte[] bytes, Class<?> manifest) {
-        Kryo kryo = new Kryo();
         Input input = new Input(new ByteArrayInputStream(bytes));
         Object object = kryo.readObject(input, manifest);
         input.close();
@@ -21,7 +27,6 @@ public class KryoSerializer extends JSerializer {
 
     @Override
     public byte[] toBinary(Object o) {
-        Kryo kryo = new Kryo();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         Output output = new Output(outputStream);
         kryo.writeObject(output, o);
