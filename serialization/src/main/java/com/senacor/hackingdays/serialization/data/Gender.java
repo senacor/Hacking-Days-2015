1f6729d40e8bc52aed703e40b20bfa02d755a398
Christian Charles <christian.charles@senacor.com>
2015-09-18 16:55:38 +0200

    reduce required buffer size

--- serialization/src/main/java/com/senacor/hackingdays/serialization/data/Gender.java
+++ serialization/src/main/java/com/senacor/hackingdays/serialization/data/Gender.java
@@ -1,11 +1,15 @@
 package com.senacor.hackingdays.serialization.data;
 
+import com.senacor.hackingdays.serialization.data.unsafe.BufferTooSmallException;
+import com.senacor.hackingdays.serialization.data.unsafe.UnsafeMemory;
+import com.senacor.hackingdays.serialization.data.unsafe.UnsafeSerializable;
+
 public enum Gender implements UnsafeSerializable {
 
     Male, Female, Disambiguous;
 
     @Override
-    public void serializeUnsafe(final UnsafeMemory memory) {
+    public void serializeUnsafe(final UnsafeMemory memory) throws BufferTooSmallException {
         memory.putInt(this.ordinal());
     }
 
