c0b071d9a0226c5db709e15a9da28d894225bf04
Hannelore Marginean <Hannelore.Marginean@senacor.com>
2015-09-18 16:14:53 +0200

    Config  von FastSerializer angepasst.

--- serialization/src/main/java/com/senacor/hackingdays/serializer/FastSerializer.java
+++ serialization/src/main/java/com/senacor/hackingdays/serializer/FastSerializer.java
@@ -1,13 +1,18 @@
 package com.senacor.hackingdays.serializer;
 
 import akka.serialization.JSerializer;
+import com.senacor.hackingdays.serialization.data.*;
 import org.nustaq.serialization.FSTConfiguration;
 
 /**
  * Created by hmarginean on 18/09/15.
  */
 public class FastSerializer extends JSerializer {
-    static FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
+    static FSTConfiguration conf = FSTConfiguration.createFastBinaryConfiguration();
+    static {
+        conf.registerClass(Activity.class,Gender.class, Location.class, Profile.class, Range.class, RelationShipStatus.class, Seeking.class);
+    }
+
 
     @Override
     public Object fromBinaryJava(byte[] bytes, Class<?> manifest) {
