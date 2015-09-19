5f96d4b50bcb1d15f11f1f9688eee26b1b9b0eed
hstamminger <henning.stamminger@senacor.com>
2015-09-19 23:48:48 +0200

    junit test rule to start an embedded mongodb

--- lmax-disruptor/src/test/java/com/senacor/hackingdays/lmax/rule/EmbeddedMongoRule.java
+++ lmax-disruptor/src/test/java/com/senacor/hackingdays/lmax/rule/EmbeddedMongoRule.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 
 import org.junit.rules.ExternalResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mongodb.MongoClient;
+import com.senacor.hackingdays.lmax.lmax.MongoJournalingDisruptorConsumerTest;
 
 import de.flapdoodle.embed.mongo.config.IMongodConfig;
 import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
@@ -16,18 +19,29 @@ import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 import de.flapdoodle.embed.process.runtime.Network;
 
 public class EmbeddedMongoRule extends ExternalResource {
+	/** Logger for this class */
+	private static final Logger LOGGER = LoggerFactory.getLogger(MongoJournalingDisruptorConsumerTest.class);
 
 	private MongodForTestsFactory mongodForTestsFactory;
 	private final String host;
 	private final int port;
 
-	public EmbeddedMongoRule(String host, int port) throws Exception {
+	public EmbeddedMongoRule() {
+		this(null, -1);
+	}
+
+	public EmbeddedMongoRule(String host, int port) {
 		this.host = host;
 		this.port = port;
 	}
 
 	@Override
 	protected void before() throws Throwable {
+		final int port = this.port > 0 ? this.port : Network.getFreeServerPort();
+		final String host = this.host != null ? this.host : Network.getLocalHost().getHostName();
+
+		LOGGER.info("starting embedded mongodb on address: {}:{}", host, port);
+
 		mongodForTestsFactory = new MongodForTestsFactory(Version.Main.PRODUCTION) {
 			@Override
 			protected IMongodConfig newMongodConfig(IFeatureAwareVersion version) throws UnknownHostException, IOException {
@@ -36,18 +50,30 @@ public class EmbeddedMongoRule extends ExternalResource {
 				.net(new Net(host, port, Network.localhostIsIPv6()))
 				.build();
 			}
+
+//			@Override
+//			public MongoClient newMongo() throws UnknownHostException, MongoException {
+//				final MongoClient template = super.newMongo();
+//				return new MongoClient(
+//					template.getAddress(),
+//					builder(template.getMongoClientOptions())
+//					.codecRegistry(fromRegistries(getDefaultCodecRegistry(), fromCodecs(new DocumentCodec())))
+//					.build());
+//			}
 		};
 	}
 
 	@Override
 	protected void after() {
+		LOGGER.info("stopping embedded mongodb on address: {}:{}", host, port);
+
 		if (mongodForTestsFactory != null) {
 			mongodForTestsFactory.shutdown();
 		}
 	}
 
-	public MongoClient getClient() {
-		return new MongoClient(host, port);
+	public MongoClient getClient() throws Exception {
+		return mongodForTestsFactory.newMongo();
 	}
 
 }
