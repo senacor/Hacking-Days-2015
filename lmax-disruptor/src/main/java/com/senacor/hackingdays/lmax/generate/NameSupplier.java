6160455cc2eda9cec1e56fdff33639eb70ed726a
Michael Omann <michael.omann@gmail.com>
2015-09-19 15:49:41 +0200

    remove queue based example

--- lmax-disruptor/src/main/java/com/senacor/hackingdays/lmax/generate/NameSupplier.java
+++ lmax-disruptor/src/main/java/com/senacor/hackingdays/lmax/generate/NameSupplier.java
@@ -3,7 +3,6 @@ package com.senacor.hackingdays.lmax.generate;
 import com.senacor.hackingdays.lmax.generate.model.Gender;
 
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public interface NameSupplier extends Function<Gender, String> {
 }
--- lmax-disruptor/src/test/java/com/senacor/hackingdays/lmax/lmax/DisruptorTest.java
+++ lmax-disruptor/src/test/java/com/senacor/hackingdays/lmax/lmax/DisruptorTest.java
@@ -4,20 +4,15 @@ import com.google.common.base.Stopwatch;
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.senacor.hackingdays.lmax.generate.ProfileGenerator;
-import com.senacor.hackingdays.lmax.generate.model.Profile;
-import com.senacor.hackingdays.lmax.queue.QueueConsumer;
-import com.senacor.hackingdays.lmax.queue.QueuePublisher;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
 
 import static junitparams.JUnitParamsRunner.$;
 
@@ -31,17 +26,19 @@ public class DisruptorTest {
     @Parameters(method = "poolSize")
     public void testDisruptor(int poolSize) throws InterruptedException {
         // Executor that will be used to construct new threads for consumers
-        Executor executor = Executors.newFixedThreadPool(poolSize);
+        ExecutorService executor = Executors.newFixedThreadPool(poolSize);
 
         // Specify the size of the ring buffer, must be power of 2.
-        int bufferSize = 1024;
-
+        int bufferSize = 4096;
         // Construct the Disruptor
         Disruptor<DisruptorEnvelope> disruptor = new Disruptor<>(DisruptorEnvelope::new, bufferSize, executor);
 
         // Connect the handler
-        Stopwatch stopwatch = Stopwatch.createUnstarted();
-        disruptor.handleEventsWith(new DisruptorConsumer(SAMPLE_SIZE, () -> stopwatch.stop()));
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        DisruptorConsumer c1 = new DisruptorConsumer(SAMPLE_SIZE, () -> countDownLatch.countDown());
+//        DisruptorConsumer c2 = new DisruptorConsumer(SAMPLE_SIZE, () -> countDownLatch.countDown());
+//        DisruptorConsumer c3 = new DisruptorConsumer(SAMPLE_SIZE, () -> countDownLatch.countDown());
+        disruptor.handleEventsWith(c1);
 
         // Start the Disruptor, starts all threads running
         disruptor.start();
@@ -49,41 +46,15 @@ public class DisruptorTest {
         // Get the ring buffer from the Disruptor to be used for publishing.
         RingBuffer<DisruptorEnvelope> ringBuffer = disruptor.getRingBuffer();
 
-        ProfileGenerator profiles = ProfileGenerator.newInstance(SAMPLE_SIZE);
-
-        stopwatch.start();
-        profiles.forEach(profile -> ringBuffer.publishEvent((envelope, sequence) -> envelope.setProfile(profile)));
-        while (stopwatch.isRunning()) {
-            TimeUnit.MILLISECONDS.sleep(100);
-        }
-        TimeUnit.MILLISECONDS.sleep(1000);
-        disruptor.shutdown();
-        System.err.println("Processed " + SAMPLE_SIZE + " profiles in " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " milliseconds.");
-    }
-
-    @Test
-    @Parameters(method = "poolSize")
-    public void testLinkedQueue(int poolSize) throws InterruptedException {
-
-        Executor executor = Executors.newFixedThreadPool(poolSize);
-
-        ConcurrentLinkedQueue<Profile> queue = new ConcurrentLinkedQueue<>();
-        QueuePublisher publisher = new QueuePublisher(queue);
-
-        CountDownLatch latch = new CountDownLatch(SAMPLE_SIZE);
-        QueueConsumer consumer = new QueueConsumer(queue, latch);
+        ProfileGenerator generator = ProfileGenerator.newInstance(SAMPLE_SIZE);
 
         Stopwatch stopwatch = Stopwatch.createStarted();
-        ProfileGenerator.newInstance(SAMPLE_SIZE).forEach(profile -> executor.execute(() -> publisher.publish(profile)));
-
-
-        IntStream.range(1, 3).forEach(i -> executor.execute(() -> consumer.poll()));
-
-
-        latch.await();
-        TimeUnit.MILLISECONDS.sleep(1000);
-        System.err.println("Processed " + SAMPLE_SIZE + " profiles in " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " milliseconds.");
-
+        generator.forEach(profile -> ringBuffer.publishEvent((envelope, sequence) -> envelope.setProfile(profile)));
+        countDownLatch.await();
+        stopwatch.stop();
+        disruptor.shutdown();
+        executor.shutdownNow();
+        System.err.println("Pool size: " + poolSize + " Time: " + stopwatch.elapsed(TimeUnit.MILLISECONDS) + " milliseconds.");
     }
 
 
@@ -91,6 +62,7 @@ public class DisruptorTest {
     static Object[] poolSize() {
         return $(
                 $(1),
+                $(2),
                 $(4),
                 $(8),
                 $(12),
