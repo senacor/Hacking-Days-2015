9a88444871b2b7254acd75c3444af5bf7b6c17a4
Alasdair Collinson <blalasaadri@googlemail.com>
2015-09-19 10:30:26 +0200

    Hazelcast-Versionen aktualisiert, Client eingebunden

---
+++ distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/GettingStartedClient.java
@@ -0,0 +1,15 @@
+package com.senacor.hackingdays.distributedcache;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class GettingStartedClient {
+    public static void main(String[] args) {
+        ClientConfig clientConfig = new ClientConfig();
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+        IMap map = client.getMap("customers");
+        System.out.println("Map Size:" + map.size());
+    }
+}
