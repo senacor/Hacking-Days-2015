8a5cb0e4c904ca94384d4bcbcbf1e7a1affe2d64
Alasdair Collinson <blalasaadri@googlemail.com>
2015-09-19 18:31:24 +0200

    Hazelcast-Config aufgeräumt

--- distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/GettingStarted.java
+++ distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/GettingStarted.java
@@ -1,7 +1,6 @@
 package com.senacor.hackingdays.distributedcache;
 
 import java.util.Map;
-import java.util.Queue;
 import java.util.UUID;
 
 import com.hazelcast.core.Hazelcast;
@@ -18,14 +17,6 @@ public class GettingStarted {
 
         // System.out.println("Profile with key 1: " + profiles.get(1));
         System.out.println("Map Size:" + hazelcastInstance.getMap("profiles").size());
-
-        Queue<Profile> queueCustomers = hazelcastInstance.getQueue("profiles");
-        // queueCustomers.offer(ProfileGenerator.newProfile());
-        // queueCustomers.offer(ProfileGenerator.newProfile());
-        // queueCustomers.offer(ProfileGenerator.newProfile());
-        System.out.println("First profile: " + queueCustomers.poll());
-        System.out.println("Second profile: " + queueCustomers.peek());
-        System.out.println("Queue size: " + queueCustomers.size());
 
         Map<String, Profile> datingProfiles = hazelcastInstance.getMap("datingProfiles");
         // ProfileGenerator.newInstance(5).stream()
