630c2e4145bba286539aa7c1b29b6b503f202f19
Alasdair Collinson <blalasaadri@googlemail.com>
2015-09-19 10:54:47 +0200

    Cluster angepasst

--- distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/GettingStartedClient.java
+++ distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/GettingStartedClient.java
@@ -4,10 +4,12 @@ import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
+import com.hazelcast.security.UsernamePasswordCredentials;
 
 public class GettingStartedClient {
     public static void main(String[] args) {
         ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setCredentials(new UsernamePasswordCredentials("sinalco", "sinalco"));
         HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
         IMap map = client.getMap("profiles");
         System.out.println("Map Size:" + map.size());
