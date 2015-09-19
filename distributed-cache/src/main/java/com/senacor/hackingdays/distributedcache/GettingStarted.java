c494682c2543bcbb26ff123fa9e0681eae4dbf28
Alasdair Collinson <blalasaadri@googlemail.com>
2015-09-19 10:05:34 +0200

    Initialer commit für Hazelcast

---
+++ distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/GettingStarted.java
@@ -0,0 +1,28 @@
+package com.senacor.hackingdays.distributedcache;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.Map;
+import java.util.Queue;
+
+public class GettingStarted {
+    public static void main( String[] args ) {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<Integer, String> customers = hazelcastInstance.getMap( "customers" );
+        customers.put( 1, "Joe" );
+        customers.put( 2, "Ali" );
+        customers.put( 3, "Avi" );
+
+        System.out.println( "Customer with key 1: " + customers.get(1) );
+        System.out.println( "Map Size:" + hazelcastInstance.size(); );
+
+        Queue<String> queueCustomers = hazelcastInstance.getQueue( "customers" );
+        queueCustomers.offer( "Tom" );
+        queueCustomers.offer( "Mary" );
+        queueCustomers.offer( "Jane" );
+        System.out.println( "First customer: " + queueCustomers.poll() );
+        System.out.println( "Second customer: "+ queueCustomers.peek() );
+        System.out.println( "Queue size: " + queueCustomers.size() );
+    }
+}
