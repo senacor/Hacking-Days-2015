5cfe06217a5fdd6fa2237dda78a4937492a77891
Markus Dobel <markus.dobel@senacor.com>
2015-09-20 09:41:57 +0200

    dbcp eingebaut, mapstore nicht mehr synchronized

--- distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/ProfileMapStore.java
+++ distributed-cache/src/main/java/com/senacor/hackingdays/distributedcache/ProfileMapStore.java
@@ -23,87 +23,86 @@ import com.senacor.hackingdays.distributedcache.generate.model.Profile;
  */
 public class ProfileMapStore implements MapStore<String, Profile>, Closeable {
 
-    private static final String DEFAULT_MAP_ID = "profile";
-    private static final String DEFAULT_DB_URL = "jdbc:h2:tcp://192.168.220.124/~/test";
-//    private static final String DEFAULT_DB_URL = "jdbc:h2:tcp://172.16.13.152/~/test";
-    public static final String PROPERTY_FILE = "maps.properties.xml";
+	private static final String DEFAULT_MAP_ID = "profile";
+	private static final String DEFAULT_DB_URL = "jdbc:h2:tcp://192.168.220.124/~/test";
+	// private static final String DEFAULT_DB_URL =
+	// "jdbc:h2:tcp://172.16.13.152/~/test";
+	public static final String PROPERTY_FILE = "maps.properties.xml";
 
-    private final String mapId;
-    private final String dbUrl;
-    private final ProfileMapper mapper;
-    private final DataSource dataSource;
+	private final String mapId;
+	private final String dbUrl;
+	private final ProfileMapper mapper;
+	private final DataSource dataSource;
 
-    public ProfileMapStore() {
-        Properties properties = getProperties();
-        mapId = properties.getProperty("mapName", DEFAULT_MAP_ID);
-        dbUrl = properties.getProperty("db-url", DEFAULT_DB_URL);
-        dataSource = createDataSource();
-        mapper = new ProfileMapper(dataSource, mapId);
-    }
+	public ProfileMapStore() {
+		Properties properties = getProperties();
+		mapId = properties.getProperty("mapName", DEFAULT_MAP_ID);
+		dbUrl = properties.getProperty("db-url", DEFAULT_DB_URL);
+		dataSource = createDataSource();
+		mapper = new ProfileMapper(dataSource, mapId);
+	}
 
-    private Properties getProperties() {
-        Properties properties = new Properties();
-        try {
-            properties.loadFromXML(getClass().getClassLoader().getResourceAsStream(PROPERTY_FILE));
-        } catch (IOException e) {
-            System.err.println("Failed to load properties from file, using defaults");
-        }
-        return properties;
-    }
+	private Properties getProperties() {
+		Properties properties = new Properties();
+		try {
+			properties.loadFromXML(getClass().getClassLoader().getResourceAsStream(PROPERTY_FILE));
+		} catch (IOException e) {
+			System.err.println("Failed to load properties from file, using defaults");
+		}
+		return properties;
+	}
 
-    @Override
-    public synchronized void store(String key, Profile value) {
-        if (mapper.getProfileById(UUID.fromString(key)) == null) {
-            mapper.insertProfile(value);
-        } else {
-            mapper.updateProfile(value);
-        }
-    }
+	@Override
+	public void store(String key, Profile value) {
+		if (mapper.getProfileById(UUID.fromString(key)) == null) {
+			mapper.insertProfile(value);
+		} else {
+			mapper.updateProfile(value);
+		}
+	}
 
-    @Override
-    public synchronized void storeAll(Map<String, Profile> map) {
-        for (Map.Entry<String, Profile> entry : map.entrySet()) {
-            store(entry.getKey(), entry.getValue());
-        }
-    }
+	@Override
+	public void storeAll(Map<String, Profile> map) {
+		for (Map.Entry<String, Profile> entry : map.entrySet()) {
+			store(entry.getKey(), entry.getValue());
+		}
+	}
 
-    @Override
-    public synchronized void delete(String key) {
-        mapper.deleteProfile(UUID.fromString(key));
-    }
+	@Override
+	public void delete(String key) {
+		mapper.deleteProfile(UUID.fromString(key));
+	}
 
-    @Override
-    public synchronized void deleteAll(Collection<String> keys) {
-        for (String key : keys) {
-            delete(key);
-        }
-    }
+	@Override
+	public void deleteAll(Collection<String> keys) {
+		for (String key : keys) {
+			delete(key);
+		}
+	}
 
-    @Override
-    public synchronized Profile load(String key) {
-        return mapper.getProfileById(UUID.fromString(key));
-    }
+	@Override
+	public Profile load(String key) {
+		return mapper.getProfileById(UUID.fromString(key));
+	}
 
-    @Override
-    public synchronized Map<String, Profile> loadAll(Collection<String> keys) {
-        Map<String, Profile> result = Maps.newHashMap();
-        keys.stream().forEach(key -> {
-            Profile profile = load(key);
-            if (profile != null) {
-                result.put(profile.getId().toString(), profile);
-            }
-        });
-        return result;
-    }
+	@Override
+	public Map<String, Profile> loadAll(Collection<String> keys) {
+		Map<String, Profile> result = Maps.newHashMap();
+		keys.stream().forEach(key -> {
+			Profile profile = load(key);
+			if (profile != null) {
+				result.put(profile.getId().toString(), profile);
+			}
+		});
+		return result;
+	}
 
-    @Override
-    public synchronized Iterable<String> loadAllKeys() {
-        return mapper.getAllIds().stream()
-                .map(UUID::toString)
-                .collect(Collectors.toList());
-    }
-    
-	private  DataSource createDataSource() {
+	@Override
+	public Iterable<String> loadAllKeys() {
+		return mapper.getAllIds().stream().map(UUID::toString).collect(Collectors.toList());
+	}
+
+	private DataSource createDataSource() {
 		JdbcDataSource dataSource = new JdbcDataSource();
 		dataSource.setURL(dbUrl);
 		JdbcConnectionPool pool = JdbcConnectionPool.create(dataSource);
@@ -111,8 +110,8 @@ public class ProfileMapStore implements MapStore<String, Profile>, Closeable {
 		return pool;
 	}
 
-    @Override
-    public void close() throws IOException {
-    	// noop
-    }
+	@Override
+	public void close() throws IOException {
+		// noop
+	}
 }
